3f0413f9b4211a60f1de94aafb241ed117c21401
atulvidyarthi29 <atulvidyarthi29@gmail.com>
2018-11-02 22:24:37 +0530

    Integrated the Clubs App

--- digital_college/clubs/views.py
+++ digital_college/clubs/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    data = Post.objects.all()
+    return render(request, 'clubs/club_basic.html', {'data': data})
+
+
+def forum(request):
+    return render(request, 'clubs/club_forum.html')
+
+
+def contacts(request):
+    pass
+
+
+def post(request):
+    return render(request, 'clubs/club_post.html')
+
+
+def gallery(request):
+    pass
--- digital_college/digital_college/urls.py
+++ digital_college/digital_college/urls.py
@@ -21,13 +21,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', user_views.website_homepage,name='website_homepage'),
+    path('', user_views.website_homepage, name='website_homepage'),
     path('users/', include('users.urls')),
     path('register', user_views.website_register, name='website_register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/website_loginpage.html'), name='website_login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/website_logoutpage.html'), name='website_logout'),
     path('admin/', admin.site.urls),
     path('base/', user_views.base),
+    path('club/', include('clubs.urls')),
 ]   
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
