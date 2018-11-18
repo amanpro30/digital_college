461bfd07fb46ec8a5d0ef5f81a3b56bca9322f4f
amanpro30 <aman.k17@iiits.in>
2018-11-18 22:46:49 +0530

    announcemnts changed

--- digital_college/announcements/views.py
+++ digital_college/announcements/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .forms import announcement_form
+from .models import announcements
+from users.models import Registered_College
 # Create your views here.
 
 def announcement(request):
+    college_name=request.user.registered_user.college_id
+    college_instance=Registered_College.objects.get(Name_Of_College=college_name)
     if request.method=='POST':
         form=announcement_form(request.POST)
         print(form.is_valid())
-
+        form.save()
+        announce=announcements.objects.all()
     else:
         form=announcement_form()
-    return render(request,'announcements/announcements.html',{'form':form})
+    return render(request,'announcements/announcements.html',{'form':form,'announcements':announce})
--- digital_college/digital_college/settings.py
+++ digital_college/digital_college/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'test',
             'USER': 'root',
-            'PASSWORD': 'Atul@123',
+            'PASSWORD': 'root',
         }
 }
 # DATABASES = {
