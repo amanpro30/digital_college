aa00d54a7d004c648956c96a88b4d9276b2a1854
atulvidyarthi29 <atulvidyarthi29@gmail.com>
2018-12-08 03:21:23 +0530

    student forum almost done

--- digital_college/forum/urls.py
+++ digital_college/forum/urls.py
@@ -5,4 +5,10 @@ app_name = "class_forum"
 
 urlpatterns = [
     path('', views.forum, name="cl_forum"),
+    path('delete/<int:post_id>/', views.delete, name="cl_delete"),
+    path('comment/<int:post_id>/', views.comment, name="cl_comment"),
+    path('details/<int:post_id>/', views.postdetails, name="cl_post"),
+    path('delcom/<int:com_id>/', views.delcom, name="cl_delcom"),
+    path('dislike/<int:post_id>/', views.dislike_post, name="cl_dislike"),
+    path('like/<int:post_id>/', views.like_post, name="cl_like"),
 ]
